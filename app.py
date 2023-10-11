321c8e115c5f29d21464688246e81f977229a688
BAPANBAIRAGYA64 <bapanbairagya96@gmail.com>
2023-10-11 13:19:04 +0530

    This is the first commit from developer-1 branch

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 print("Hello, World!")
+
+## lets print number from 1 to n
+def print_n(n: int):
+    ## base case
+    if n <= 0:
+        return
+    
+    print_n(n-1)
+    print(n)
+
+    return
